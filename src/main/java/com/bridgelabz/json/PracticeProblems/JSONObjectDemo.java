6f1180923b0ee1477af659f68bb7283322b9bfe5
ankush1038 <ankusharma1029@gmail.com>
2025-04-09 22:44:57 +0530

    [Ankush] Add: JSON object created

---
+++ src/main/java/com/bridgelabz/json/PracticeProblems/JSONObjectDemo.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.json.PracticeProblems;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+    class JSONObjectDemo {
+        public static void main(String[] args) {
+            JSONObject student = new JSONObject();
+            student.put("name", "Rahul Mehta");
+            student.put("age", 20);
+
+            JSONArray subjects = new JSONArray();
+            subjects.put("Math");
+            subjects.put("Science");
+            subjects.put("English");
+
+            student.put("subjects", subjects);
+
+            System.out.println(student.toString(2)); // Pretty print
+        }
+    }
+
+
